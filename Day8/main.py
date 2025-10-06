93d067fa34a0dbbbddc608e0187fe845eebc4f0f
kelvinlawsonw <kelvinlaw8@gmail.com>
2025-10-06 13:53:37 -0400

    Day 8 addition

--- Day8/main.py
+++ Day8/main.py
@@ -1,5 +1,9 @@
 # Caesar Cipher
 
+# import art
+# print(art.logo)
+
+
 print("Welcome to the Caesar Cipher Encoder/Decoder!")
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
@@ -9,6 +13,9 @@ shift = int(input("Type the shift number:\n"))
 def encrypt(plain_text, shift_amount):
     cipher_text = ""
     for letter in plain_text:
+        if letter not in alphabet:
+            cipher_text += letter
+            continue
         shifted_position = alphabet.index(letter) + shift_amount
         shifted_position %= len(alphabet)
         cipher_text += alphabet[shifted_position]
@@ -17,10 +24,14 @@ def encrypt(plain_text, shift_amount):
 def decrypt(cipher_text, shift_amount):
     plain_text = ""
     for letter in cipher_text:
+        if letter not in alphabet:
+            plain_text += letter
+            continue
         shifted_position = alphabet.index(letter) - shift_amount
         shifted_position %= len(alphabet)
         plain_text += alphabet[shifted_position]
     print(f"The decoded text is {plain_text}")
+    
 
 if direction == "encode":
     encrypt(plain_text=text, shift_amount=shift)
